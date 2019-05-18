2b810d950075c501af7b102cd34a686cfc0afe19
张佳俊 <zhangjiajun@zhangjiajundemacbook-pro.local>
2019-05-18 14:19:11 +0800

    hello c++

---
+++ LearningC++/LearningC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearningC++
+//
+//  Created by 张佳俊 on 2019/5/18.
+//  Copyright © 2019年 张佳俊. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
