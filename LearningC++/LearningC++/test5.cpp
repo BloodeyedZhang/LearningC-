e6770aea45db64c545a443e6cf6d2482fd8664c8
BloodeyedZhang <512867879@qq.com>
2019-05-20 23:27:15 +0800

    command.

---
+++ LearningC++/LearningC++/test5.cpp
@@ -0,0 +1,66 @@
+//
+//  test5.cpp
+//  LearningC++
+//
+//  Created by 张佳俊 on 2019/5/20.
+//  Copyright © 2019年 张佳俊. All rights reserved.
+//
+
+#include <stdio.h>
+
+// 可触发的游戏命令
+class Command {
+public:
+    virtual ~Command() {};
+    virtual void execute(GameActor& actor) = 0;
+};
+
+class JumpCommand : Command {
+public:
+    virtual void execute(GameActor& actor) {
+        actor.jump();
+    }
+};
+
+class FireCommand : Command {
+public:
+    virtual void execute(GameActor& actor) {
+        actor.fireGun();
+    }
+};
+
+class LurCommand : Command {
+public:
+    virtual void execute(GameActor& actor) {
+        actor.lurchIneffectively();
+    }
+}
+
+class SwapWeaponCommand : Command {
+public:
+    virtual void execute(GameActor& actor) {
+        actir.swapWeapon();
+    }
+}
+
+class InputHandler {
+public:
+    Command* handleInput();
+    
+private:
+    Command* buttonX_;
+    Command* buttonY_;
+    Command* buttonA_;
+    Command* buttonB_;
+    
+};
+
+Command* InputHandler::handleInput() {
+    if (isPressed(BUTTON_X)) return buttonX_;
+    else if (isPressed(BUTTON_Y)) return buttonY_;
+    else if (isPressed(BUTTON_A)) return buttonA_;
+    else if (isPressed(BUTTON_B)) return buttonB_;
+    
+    // Nothing pressed, so do nothing
+    return null;
+}
